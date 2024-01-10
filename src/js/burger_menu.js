edf1d614234ec32e62cba9dbf09b17eb04e3f54c
Rostyslav <124372762+hockag@users.noreply.github.com>
2024-01-10 13:49:48 +0200

    fix bugs

--- src/js/burger_menu.js
+++ src/js/burger_menu.js
@@ -1,4 +1,6 @@
 document.querySelector('.burger-menu').addEventListener('click', function () {
     this.classList.toggle('active')
     document.querySelector('.header__nav').classList.toggle('open');
+    document.querySelector('.header__right').classList.toggle('open');
+    document.querySelector('.social-link').classList.toggle('open');
 })
